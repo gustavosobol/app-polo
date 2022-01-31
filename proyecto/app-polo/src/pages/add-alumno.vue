7be69255e04550f8524211e553e48f1195cf46bc
Gustavo Sobol <gustavo.javier.sobol@gmail.com>
2022-01-31 17:33:06 -0300

    agregamos la pantalla principal de alumnos

---
+++ proyecto/app-polo/src/pages/add-alumno.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page class="flex flex-">
+    <q-form action="https://www.google.com" method="post" @submit.prevent="onSubmit">
+      <q-input name="firstname" v-model="firstname">
+      </q-input>
+
+        <div class="q-pa-md">
+          <q-date
+            v-model="date"
+            :events="events"
+            event-color="orange"
+          />
+        </div>
+    </q-form>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex',
+  data() {
+    return {
+      firstname: null
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      console.log('@submit - do something here', evt)
+      evt.target.submit()
+    }
+  },
+  setup () {
+    return {
+      splitterModel: ref(50),
+      date: ref('2019/02/01'),
+      events: [ '2019/02/01', '2019/02/05', '2019/02/06' ]
+    }
+  }
+})
+
+</script>
--- proyecto/app-polo/src/router/routes.js
+++ proyecto/app-polo/src/router/routes.js
@@ -7,7 +7,13 @@ const routes = [
       { path: '', component: () => import('pages/Index.vue') }
     ]
   },
-
+  {
+    path: '/alumnos',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/add-alumno.vue') }
+    ]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
