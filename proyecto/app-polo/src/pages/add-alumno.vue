c8bc2efa95622d449c413acc9074b8ee2dc42be4
Gustavo Sobol <gustavo.javier.sobol@gmail.com>
2022-02-01 00:56:05 -0300

    add default value date calendar

--- proyecto/app-polo/src/pages/add-alumno.vue
+++ proyecto/app-polo/src/pages/add-alumno.vue
@@ -4,13 +4,21 @@
       <q-input name="firstname" v-model="firstname">
       </q-input>
 
-        <div class="q-pa-md">
-          <q-date
-            v-model="date"
-            :events="events"
-            event-color="orange"
-          />
-        </div>
+<div class="q-pa-md" style="max-width: 300px">
+    <q-input filled v-model="date" mask="date" :rules="['date']">
+      <template v-slot:append>
+        <q-icon name="event" class="cursor-pointer">
+          <q-popup-proxy ref="qDateProxy" cover transition-show="scale" transition-hide="scale">
+            <q-date v-model="date">
+              <div class="row items-center justify-end">
+                <q-btn v-close-popup label="Close" color="primary" flat />
+              </div>
+            </q-date>
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+    </q-input>
+  </div>
     </q-form>
   </q-page>
 </template>
@@ -18,6 +26,12 @@
 <script>
 import { defineComponent, ref } from 'vue';
 
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = yyyy+ '/' + mm + '/' + dd  ;
 export default defineComponent({
   name: 'PageIndex',
   data() {
@@ -26,16 +40,17 @@ export default defineComponent({
     }
   },
   methods: {
+    
     onSubmit (evt) {
       console.log('@submit - do something here', evt)
       evt.target.submit()
     }
   },
+  
   setup () {
     return {
       splitterModel: ref(50),
-      date: ref('2019/02/01'),
-      events: [ '2019/02/01', '2019/02/05', '2019/02/06' ]
+      date: ref(today)
     }
   }
 })
