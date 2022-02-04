1540273e267d3776edac0d6af3d2a93d8e88e8aa
Gustavo Sobol <gustavo.javier.sobol@gmail.com>
2022-02-03 21:56:21 -0300

    trabajando con tablas

--- proyecto/app-polo/src/router/routes.js
+++ proyecto/app-polo/src/router/routes.js
@@ -7,7 +7,9 @@ const routes = [
       { path: '', component: () => import('pages/Index.vue') },
       { path: '/alumnos', component: () => import('pages/add-alumno.vue') },
       { path: '/plantilla', component: () => import('pages/plantilla.vue') },
-      { path: '/notifi', component: () => import('pages/notifi.vue') }
+      { path: '/notifi', component: () => import('pages/notifi.vue') },
+      { path: '/tabla', component: () => import('pages/tabla.vue') },
+      { path: '/tabla2', component: () => import('pages/tabla2.vue') }
     ]
   },
   // Always leave this as last one,
