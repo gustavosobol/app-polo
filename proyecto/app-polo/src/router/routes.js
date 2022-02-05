25a132569796abd90598627f3243d16e76a9fd81
Sun <santiago.lizondo@gmail.com>
2022-02-04 22:26:22 -0300

    plantilla 3

--- proyecto/app-polo/src/router/routes.js
+++ proyecto/app-polo/src/router/routes.js
@@ -7,9 +7,10 @@ const routes = [
       { path: '', component: () => import('pages/Index.vue') },
       { path: '/alumnos', component: () => import('pages/add-alumno.vue') },
       { path: '/plantilla', component: () => import('pages/plantilla.vue') },
-      { path: '/notifi', component: () => import('pages/notifi.vue') },
+     // { path: '/notifi', component: () => import('pages/notifi.vue') },
       { path: '/tabla', component: () => import('pages/tabla.vue') },
-      { path: '/tabla2', component: () => import('pages/tabla2.vue') }
+      { path: '/tabla2', component: () => import('pages/tabla2.vue') },
+      { path: '/tabla3', component: () => import('pages/tabla3.vue') }
     ]
   },
   // Always leave this as last one,
