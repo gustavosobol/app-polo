312138b292f0d277c9b35b7020db9eae795a1495
Gustavo Sobol <gustavo.javier.sobol@gmail.com>
2022-02-18 15:04:55 -0300

    arreglo dockerfile

--- proyecto/app-polo/src/pages/students/add-students.vue
+++ proyecto/app-polo/src/pages/students/add-students.vue
@@ -3,17 +3,17 @@
     <h6 class="q-my-sm">Agregar alumno {{ $route.params.idCurso }}</h6>
     <!--     <div class="col-3 col-sm-3 q-gutter-sm q-col-gutter-sm">
  -->
-    <formStudents />
+    <formStudentsVue />
     <!-- </div> -->
   </q-page>
 </template>
 
 <script>
-import formStudents from "components/students/formStudents.vue";
+import formStudentsVue from "../../components/students/formStudents.vue";
 
 export default {
   components: {
-    formStudents,
+    formStudentsVue,
   },
   setup() {},
 };
